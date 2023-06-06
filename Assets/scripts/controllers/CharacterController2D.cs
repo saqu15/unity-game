b9782fc5d1d2662380410236a2c73e6e3e4691f1
PawelBieleckiUS <pawel.bielecki@smcebi.edu.pl>
2023-06-06 16:32:35 +0200

    usprawnione animacje kontrolera

--- Assets/scripts/controllers/CharacterController2D.cs
+++ Assets/scripts/controllers/CharacterController2D.cs
@@ -9,10 +9,11 @@ public class CharacterController2D : MonoBehaviour
 
     private Rigidbody2D rb;
     private bool isJumping = false;
-
     private void Start()
     {
+        Animator animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
+        animator.SetBool("IsFacingRight", true);
     }
 
     private void Update()
@@ -20,25 +21,40 @@ public class CharacterController2D : MonoBehaviour
         float moveX = Input.GetAxis("Horizontal");
         rb.velocity = new Vector2(moveX * moveSpeed, rb.velocity.y);
 
-        float speed = Mathf.Abs(moveX);
-        
         Animator animator = GetComponent<Animator>();
+        bool isMovingRight = Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D);
+        bool isMovingLeft = Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A);
+        
+
         if (moveX > 0)
         {
-            animator.SetBool("IsFacingRight", true); 
+            animator.SetBool("IsFacingRight", true);
         }
         else if (moveX < 0)
         {
-            animator.SetBool("IsFacingRight", false);
+            animator.SetBool("IsFacingRight", false); 
         }
-
+        if (isMovingRight)
+        {
+            animator.SetBool("IsMovingRight", true);
+            animator.SetBool("IsMovingLeft", false);
+        }
+        else if (isMovingLeft)
+        {
+            animator.SetBool("IsMovingRight", false);
+            animator.SetBool("IsMovingLeft", true);
+        }
+        else
+        {
+            animator.SetBool("IsMovingRight", false);
+            animator.SetBool("IsMovingLeft", false);
+        }
         if (Input.GetButtonDown("Jump") && !isJumping)
         {
             rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
             isJumping = true;
             animator.SetBool("IsJumping", true);
         }
-        animator.SetFloat("Speed", speed);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
